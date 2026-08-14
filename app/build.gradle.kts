58160b713d95b48aa4e648e7cbd7973207ac1a3e
jagr <jagr.developer@gmail.com>
2026-08-13 23:41:48 -0600

    chore: actualizar la versión de la aplicación
    - Se incrementó `versionCode` de 9 a 10.
    - Se actualizó `versionName` de "1.0.8.108" a "1.0.8.109".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.jagr.fridamusic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.0.8.108"
+        versionCode = 10
+        versionName = "1.0.8.109"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
