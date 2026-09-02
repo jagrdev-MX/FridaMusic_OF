08101d7ee1e8586729725e452b9e3571405492ce
jagr <jagr.developer@gmail.com>
2026-09-02 06:48:44 -0600

    chore: update application version
    - Increment `versionCode` from 14 to 15.
    - Update `versionName` from "1.2.13.251" to "1.2.15.251".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.jagr.fridamusic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.2.13.251"
+        versionCode = 15
+        versionName = "1.2.15.251"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
